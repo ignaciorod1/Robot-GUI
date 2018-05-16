31d2c21cbfe0a158ba695c4c84853e49c42a404d
Ignacio <ignacio.rodriguez.naranjo@alumnos.upm.es>
2018-05-16 17:32:38 +0200

    Analog read through serial communication
    An analog value from a potentiometer is read from the A0 input pin. It is maped by the arduino code to be shown in a percentage. This valueis later stored in the C# GUI and shown in a text label.

--- GUI/GUI/Form1.Designer.cs
+++ GUI/GUI/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -40,7 +41,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Font = new System.Drawing.Font("Bebas Kai", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button1.Location = new System.Drawing.Point(112, 127);
+            this.button1.Location = new System.Drawing.Point(115, 80);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 200);
             this.button1.TabIndex = 0;
@@ -51,13 +52,14 @@
             // serialPort1
             // 
             this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Font = new System.Drawing.Font("Bebas Kai", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(388, 127);
+            this.button2.Location = new System.Drawing.Point(389, 80);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 200);
             this.button2.TabIndex = 1;
@@ -65,12 +67,22 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(702, 633);
+            this.ClientSize = new System.Drawing.Size(702, 446);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -78,6 +90,7 @@
             this.Name = "Form1";
             this.Text = "Iron Plant";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +99,7 @@
         private System.Windows.Forms.Button button1;
         public System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GUI/GUI/Form1.cs
+++ GUI/GUI/Form1.cs
@@ -12,12 +12,16 @@ namespace GUI
 {
     public partial class Form1 : Form
     {
+        public string potVal;
+        
         public Form1()
         {
+            potVal = "0";         //inicialización del valor del potenciometro
             InitializeComponent();
             serialPort1.Open();
         }
 
+        
         private void button1_Click(object sender, EventArgs e)
         {
             serialPort1.Write("A");
@@ -26,6 +30,23 @@ namespace GUI
         private void button2_Click(object sender, EventArgs e)
         {
             serialPort1.Write("B");
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string potVal = serialPort1.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), potVal);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private delegate void LineReceivedEvent(string line);
+
+        private void LineReceived(string pot)
+        {
+            label1.Text = pot;
         }
     }
 }
